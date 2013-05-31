b6a59d390da6bad760c3700f2d0063cd8792d162
Lukasz Rozek <lukasz.rozek@consileon.pl>
2013-05-31 09:33:19 +0200

    changes to arrays example

--- src/main/java/pl/lrozek/szkolenia/array/ArrayIsAnObject.java
+++ src/main/java/pl/lrozek/szkolenia/array/ArrayIsAnObject.java
@@ -1,13 +1,16 @@
 package pl.lrozek.szkolenia.array;
 
+import java.util.Arrays;
+
 public class ArrayIsAnObject {
 
     public static void main( String[] args ) {
         String array[] = { "Ala", "ma", "kota" };
+        String array1[] = { "Ala", "ma", "kota", null };
 
-        System.out.println( "array.toString(): " + array.toString() );
-        System.out.println( "array.hashCode(): " + array.hashCode() );
-        System.out.println( "array.getClass(): " + array.getClass() );
+        System.out.println( array == array1 );
+        System.out.println( Arrays.equals( array, array1 ) );
+
     }
 
 }
